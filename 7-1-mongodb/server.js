7491f26ff49f71a2807e011867b3b49086351eb7
LenaAshqar <s202282660@kfupm.edu.sa>
2025-11-17 12:05:13 +0300

    todo 1-6

--- 7-1-mongodb/server.js
+++ 7-1-mongodb/server.js
@@ -169,22 +169,45 @@
 
 import mongoose from "mongoose";
 
-// establish connection
+mongoose.connect("mongodb+srv://dbUser:<dbUser1234>@cluster0.uggsxus.mongodb.net/")
 
 
 // define schema
-
+const studentSchema = new mongoose.Schema({
+         name: String,
+         age: Number,
+         major: String
+      });
+      const Student = mongoose.model("Student", studentSchema);
 
 // create document
+async function createStudents() {
+      await Student.insertMany([
+         { name: "Ali", age: 21, major: "CS" },
+         { name: "Sara", age: 23, major: "SE" }
+      ]);
+      console.log("✅ Inserted");
+      }
+      createStudents();
 
-
-// read document
-
+      // read document
+async function readStudents() {
+         const all = await Student.find();
+         console.log(all);
+      }
+      readStudents();
 
 // update document
-
+async function updateStudent() {
+         await Student.updateOne({ name: "Ali" }, { age: 22 });
+         console.log("✅ Updated Ali");
+      }
 
 // delete document
+async function deleteStudent() {
+         await Student.deleteOne({ name: "Sara" });
+      console.log("✅ Deleted Sara");
+      }
 
 
 
